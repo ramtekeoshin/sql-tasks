5079ce0c4ec10d99a83865cee768012f2938a7e0
ramtekeoshin <ramtekeoshin166@gmail.com>
2024-10-10 11:49:40 +0530

    file

---
+++ 25-2024.sql
@@ -0,0 +1,11 @@
+insert into employee_details1(FirstName,LastName,Email,PhoneNumber,HireDate,Salary,DepartmentID,Is_Active,JobTitle) values 
+('Aditi','Sharm','aditi@gmail.com',765342983,'2023-07-15',65000,8,True,'Software Engineer'),
+('Priya','singh','priya@gmail.com',985045389,'2023-09-12',45000,9,'false','Financial Analyst'),
+('Sameer','patel','sameer@gmail.com',9834257982,'2023-07-19',35000,7,True,'QA Engineer'),
+('Kavita','mehta','mehta1@gmail.com',984753265,'2024-05-13',25000,3,'false','Marketing Executive'),
+('Sunita','yadav','sunita@gmail.com',95744234232,'2022-06-13',10000,2,'True','Administrative Assistant'),
+('Vikram','joshi','jsohi@gmail.com',98472876677,'2021-06-13',65900,3,'false','HR Manager'),
+('Pankaj','deshmukh','desh@gmail.com',9234617657,'2022-06-15',78990,4,'false','Recruitment Specialist'),
+('Ajay','bansal','ajay@gmail.com',9874652137,'2024-08-13',70000,2,'true','Data Analyst'),
+('Divya','agrawal','divya@gmail.com',98756432135,'2021-04-13',42000,3,'false','Content Writer'),
+('Neha','patil','patil@gmail.com',8266442477,'2023-02-13',12000,3,'true','Business Analyst');
